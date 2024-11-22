eb16318dfe5ef7f719be75616ee32a4208c564f5
plyre <plyres64@gmail.com>
2024-11-22 16:36:05 +0100

    Première version de la todoList

--- todoApp/src/app/home/home.page.ts
+++ todoApp/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToDoList } from '../to-do-list';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList: ToDoList[] = [];
+  newTodoText: string = '';
 
   constructor() {}
 
+  addTodo() {
+    if (this.newTodoText !== '') {
+      this.todoList.push({
+        toDoContent: this.newTodoText 
+      }
+      );
+      this.newTodoText = '';
+    }
+  }
+
+  removeToDo(){
+    this.todoList = this.todoList
+  }
 }
